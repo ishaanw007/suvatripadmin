72bd9cd6c72e46317287d3663126f1fd8c3f63a0
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2023-12-11 11:28:48 +0530

    refector : nested routes url fixed

--- src/Components/NestedRoutes.js
+++ src/Components/NestedRoutes.js
@@ -22,15 +22,15 @@ function NestedRoutes() {
         <Routes>
             <Route path='/' element={<Contactform />} />
             <Route path='basic' element={< Basic />} />
-            <Route path='basic/photos' element={< Picture />} />
-            <Route path='basic/photos/facility' element={< Facility />} />
-            <Route path='basic/photos/facility/room-setup' element={< RoomSetUp />} />
-            <Route path='basic/photos/facility/room-setup/rate' element={< Rate />} />
-            <Route path='basic/photos/facility/room-setup/rate/hotel-rules' element={< Hotel />} />
-            <Route path='basic/photos/facility/room-setup/rate/hotel-rules/payment' element={< Payment />} />
-            <Route path='basic/photos/facility/room-setup/rate/hotel-rules/payment/park' element={< Park />} />
-            <Route path='basic/photos/facility/room-setup/rate/hotel-rules/payment/park/transportation' element={< Transportation />} />
-            <Route path='basic/photos/facility/room-setup/rate/hotel-rules/payment/park/transportation/document' element={< Document />} />
+            <Route path='photos' element={< Picture />} />
+            <Route path='facility' element={< Facility />} />
+            <Route path='room-setup' element={< RoomSetUp />} />
+            <Route path='rate' element={< Rate />} />
+            <Route path='hotel-rules' element={< Hotel />} />
+            <Route path='payment' element={< Payment />} />
+            <Route path='park' element={< Park />} />
+            <Route path='transportation' element={< Transportation />} />
+            <Route path='document' element={< Document />} />
         </Routes>
 
     )
